b84df1ea6898c7b209d27b46c35059b59642587e
DKD28 <155072207+DKD28@users.noreply.github.com>
2023-12-28 15:22:47 +0300

    Update Exercise 2.py

--- Exercises/Exercise 2.py
+++ Exercises/Exercise 2.py
@@ -2,10 +2,6 @@ A = int(input("Введите число А "))
 B = int(input("Введите число В "))
 p = 0
 for i in range(A,B+1):
-    if A >=B:
-        print("Число А больше числа В")
-        break
-    else:
         print(i)
         p+=1
 print ("Общее количество чисел = ", p)
